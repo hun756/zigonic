5254ea49f07030d1878fa1913db74cff0ea56d1a
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2023-11-01 20:23:41 +0300

    Refactor build systemRefactor Zig Build System for Improved Test and Run Configurations
    This commit encapsulates a significant refactorization of the Zig build system utilized in the project, aiming to enhance the configurations for testing and running the application. Key changes include:
    
    - Modularization: Extracted logic into separate functions `createExecutable`, `setupRunStep`, and `setupTestStep` to enhance code organization and readability.
    - Test File Struct: Introduced a `TestFile` struct to encapsulate test file path information, streamlining the setup of test steps.
    - Run and Test Step Setup: Refined the setup processes for run and test steps, making them more intuitive and less error-prone.

--- build.zig
+++ build.zig
@@ -1,19 +1,33 @@
 const std = @import("std");
 
+const TestFile = struct {
+    path: []const u8,
+};
+
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const exe = b.addExecutable(.{
-        .name = "zigonic",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
+    const exe = createExecutable(b, "zigonic", "src/main.zig");
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
+    setupRunStep(b, run_cmd);
 
+    const tests = [_]TestFile{
+        TestFile{ .path = "src/tests/all_of_test.zig" },
+        TestFile{ .path = "src/tests/max_element_test.zig" },
+    };
+
+    setupTestStep(b, &tests);
+}
+
+fn createExecutable(b: *std.Build, name: []const u8, rootSource: []const u8) *std.Build.Step.Compile {
+    return b.addExecutable(.{
+        .name = name,
+        .root_source_file = .{ .path = rootSource },
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+}
+
+fn setupRunStep(b: *std.Build, run_cmd: *std.Build.Step.Run) void {
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
@@ -22,23 +36,17 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
 
-    const tests = [_][]const u8{
-        "src/tests/all_of_test.zig",
-        "src/tests/max_element_test.zig",
-    };
-
+fn setupTestStep(b: *std.Build, tests: []const TestFile) void {
     const zigonicmodule = b.createModule(.{
         .source_file = std.Build.LazyPath.relative("src/zigonic.zig"),
     });
 
     const all_tests_step = b.step("test", "Run all tests");
-    for (tests, 0..) |test_file, i| {
-        _ = i;
+    for (tests) |test_file| {
         const test_exe = b.addTest(.{
-            .root_source_file = .{ .path = test_file },
-            .target = target,
-            .optimize = optimize,
+            .root_source_file = .{ .path = test_file.path },
         });
         test_exe.addModule("zigonic", zigonicmodule);
         const test_step = b.addRunArtifact(test_exe);
